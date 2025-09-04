3fd4763850d20aea31fc8047f60ba9314e9d33ec
Srujan kumar <srujan.psj@gmai.com>
2025-09-04 10:02:59 -0400

    Date and time

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -3,7 +3,7 @@
 Number=$1
 if [ $Number -gt 100 ]
 then
-echo "$Number is greater than 100"
+    echo "$Number is greater than 100"
 else
-echo "$Number is less than 100"
+    echo "$Number is less than 100"
 fi
