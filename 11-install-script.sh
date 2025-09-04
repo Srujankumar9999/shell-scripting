4c484cc6c1ca38fafb55294f8b065101a991cbd7
Srujan kumar <srujan.psj@gmai.com>
2025-09-04 12:07:18 -0400

    Date and time

--- 11-install-script.sh
+++ 11-install-script.sh
@@ -6,5 +6,30 @@ then
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1 #other than 0
 fi
+dnf list installed mysql
+if [$? -ne 0]
+then 
 
-dnf install mysql -y
+    dnf install mysql -y
+    if [$? -ne 0]
+    then 
+        echo "MYSql Install will fail"
+    else
+        echo "MYSql install will success"
+    fi
+else 
+echo "MYsql is already installed"
+
+dnf list installed Git
+if [$? -ne 0]
+then 
+
+    dnf install Git -y
+    if [$? -ne 0]
+    then 
+        echo "MYSql Install will fail"
+    else
+        echo "MYSql install will success"
+    fi
+else 
+echo "Git is already installed"
