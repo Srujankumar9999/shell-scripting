c7dec6356e6850b5838b83a7c79aae1e2d5ab3ed
Srujan kumar <srujan.psj@gmai.com>
2025-09-04 09:25:38 -0400

    Date and time

--- 08-array.sh
+++ 08-array.sh
@@ -2,6 +2,6 @@
 
 movies=("RRR","Devara","ASVR")
 
-echo "1st movie is ${movies[0]}"
-echo "2nd movie is ${movies[0]}"
-echo "3rd movie is ${movies[0]}"
+echo "1st movie: ${movies[0]}"
+echo "2nd movie: ${movies[1]}"
+echo "3rd movie: ${movies[2]}"
